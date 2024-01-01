ceb150e6a9b917bb6b1aedd3c1daad50de009027
mion/Mion <m.higaki@4nonome.com>
2024-01-01 14:40:18 +0900

    Atoms-Title

---
+++ src/components/Atoms/Title/index.jsx
@@ -0,0 +1,19 @@
+import COLOR from "../../../variables/color";
+import TEXT from "../../../variables/texts";
+import BREAKPOINT from "../../../variables/breakpoint";
+import FONT_FAMILY from "../../../variables/font_family";
+import styled from "styled-components";
+import React from "react";
+
+export const Title = () => {
+  return <StyledText>SIMPLE TODO APP</StyledText>;
+};
+const StyledText = styled.div`
+  color: ${COLOR.WHITE};
+  font-family: ${FONT_FAMILY.ROBOTO};
+  ${TEXT.L};
+  @media screen and (max-width: ${BREAKPOINT.MEDIUM}) {
+    font-family: ${FONT_FAMILY.NOTO_SANS};
+    ${TEXT.M};
+  }
+`;
