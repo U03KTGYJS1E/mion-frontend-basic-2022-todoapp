5d7b3e23633d6c760c9e8c4a12434c6692d69f4d
mion/Mion <m.higaki@4nonome.com>
2024-01-02 14:31:16 +0900

    Atoms-EditButton

---
+++ src/components/Atoms/EditButton/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import pencil from "../../../assets/svg/pencil.svg";
+import styled from "styled-components";
+import COLOR from "../../../variables/color";
+
+export const EditButton = ({ onClick }) => {
+  return (
+    <Button onClick={onClick}>
+      <img src={pencil} />
+    </Button>
+  );
+};
+export default EditButton;
+
+const Button = styled.button`
+  border: none;
+  cursor: pointer;
+  width: 20px;
+  height: 20px;
+  background: none;
+  padding: 0;
+  border-radius: 50%;
+  transition: 0.2s;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+
+  &:hover {
+    background-color: ${COLOR.LIGHT_GRAY_TRANSLUCENT};
+  }
+`;
